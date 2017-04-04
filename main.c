4d573c7ec9c75aae3854dd8d06bba7ba51055156
Adriano M. Martins <Adriano_mqsmts@outlook.com.br>
2017-04-04 11:23:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main(void)
+#include <stdlib.h>
+#include "TST.h"
+
+int main()
 {
-	printf("TST!\n");
-	return 0;
+    Node *root = NULL;
+
+    insert(&root, "cat");
+    insert(&root, "cats");
+    insert(&root, "up");
+    insert(&root, "bug");
+
+    printf("Following is traversal of ternary search tree\n");
+    traverseTST(root);
+
+    printf("\nFollowing are search results for cats, bu and cat respectively\n");
+    search(root, "cats");
+    search(root, "bu");
+    search(root, "cat");
+
+    return 0;
 }
